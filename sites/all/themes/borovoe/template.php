8aec6ba5917ee930dbd86ac36e613fd5c2f78685
mrded <dmitry@demenchuk.me>
2013-02-26 19:10:43 +0000

    Improve theme

--- sites/all/themes/borovoe/template.php
+++ sites/all/themes/borovoe/template.php
@@ -1,1 +1,8 @@
 <?php
+
+/**
+ * Bootstrap theme wrapper function for the primary menu links
+ */
+function borovoe_menu_tree__primary(&$variables) {
+  return '<ul class="nav nav-pills nav-stacked">' . $variables['tree'] . '</ul>';
+}
--- sites/all/themes/borovoe/templates/page.tpl.php
+++ sites/all/themes/borovoe/templates/page.tpl.php
@@ -1,29 +1,48 @@
 <div class="main-container container">
   <header></header>
 
-  <section>
-    <?php if (!empty($page['highlighted'])): ?>
-      <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-    <?php endif; ?>
-    <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
-    <a id="main-content"></a>
-    <?php print render($title_prefix); ?>
-    <?php if (!empty($title)): ?>
-      <h1 class="page-header"><?php print $title; ?></h1>
-    <?php endif; ?>
-    <?php print render($title_suffix); ?>
-    <?php print $messages; ?>
-    <?php if (!empty($tabs)): ?>
-      <?php print render($tabs); ?>
-    <?php endif; ?>
-    <?php if (!empty($page['help'])): ?>
-      <div class="well"><?php print render($page['help']); ?></div>
-    <?php endif; ?>
-    <?php if (!empty($action_links)): ?>
-      <ul class="action-links"><?php print render($action_links); ?></ul>
-    <?php endif; ?>
-    <?php print render($page['content']); ?>
-  </section>
+  <div class="container-fluid">
+    <div class="row-fluid">
+      <div class="span3">
+        <?php if (!empty($logo)): ?>
+          <a class="logo pull-left" href="<?php print $front_page; ?>" title="<?php print t('Home'); ?>">
+            <img src="<?php print $logo; ?>" alt="<?php print t('Home'); ?>" />
+          </a>
+        <?php endif; ?>
+
+        <?php if (!empty($primary_nav)): ?>
+          <?php print render($primary_nav); ?>
+        <?php endif; ?>
+        <!--Sidebar content-->
+      </div>
+      <div class="span8">
+        <section>
+          <?php if (!empty($page['highlighted'])): ?>
+            <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
+          <?php endif; ?>
+          <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
+          <a id="main-content"></a>
+          <?php print render($title_prefix); ?>
+          <?php if (!empty($title)): ?>
+            <h1 class="page-header"><?php print $title; ?></h1>
+          <?php endif; ?>
+          <?php print render($title_suffix); ?>
+          <?php print $messages; ?>
+          <?php if (!empty($tabs)): ?>
+            <?php print render($tabs); ?>
+          <?php endif; ?>
+          <?php if (!empty($page['help'])): ?>
+            <div class="well"><?php print render($page['help']); ?></div>
+          <?php endif; ?>
+          <?php if (!empty($action_links)): ?>
+            <ul class="action-links"><?php print render($action_links); ?></ul>
+          <?php endif; ?>
+          <?php print render($page['content']); ?>
+        </section>
+        <!--Body content-->
+      </div>
+    </div>
+  </div>
 
   <footer class="footer container">
     <?php print render($page['footer']); ?>
