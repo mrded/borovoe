5290f6969aed3f1ebf6fa4bf024a0a03bd9a3fcf
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-04-03 20:48:55 +0100

    Fix menu active item.

--- sites/all/themes/borovoe/template.php
+++ sites/all/themes/borovoe/template.php
@@ -6,3 +6,18 @@
 function borovoe_menu_tree__primary(&$variables) {
   return '<ul class="nav nav-pills nav-stacked">' . $variables['tree'] . '</ul>';
 }
+
+
+function borovoe_preprocess_page(&$vars, $hooks) {
+  // Add 'active' class to active menu items.
+  foreach ($vars['primary_nav'] as &$item) {
+    if (isset($item['#href'])) {
+      $link_is_same = ($item['#href'] == $_GET['q']);
+      $is_front_page = ($item['#href'] == "<front>" && drupal_is_front_page());
+
+      if ($link_is_same || $is_front_page) {
+        $item['#attributes']['class'][] = 'active';
+      }
+    }
+  }
+}
