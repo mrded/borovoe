81dbcc4b6646d56d1e3118551dc3f9bda006b1eb
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-04-07 13:52:13 +0100

    Split by category

--- sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_onecol/bootstrap-onecol.tpl.php
+++ sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_onecol/bootstrap-onecol.tpl.php
@@ -1,4 +1,4 @@
-<div class="panel-display row-fluid bootstrap-onecol" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
+<div class="panel-display row-fluid bootstrap-onecol-12" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
   <div class="panel-panel span12">
     <?php print $content['middle']; ?>
   </div>
