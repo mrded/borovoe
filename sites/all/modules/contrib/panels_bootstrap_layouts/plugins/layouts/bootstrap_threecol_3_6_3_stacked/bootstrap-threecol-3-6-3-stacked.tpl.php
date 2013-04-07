59a75f1d974c536e3d2c305f24710ff31f3fc7a0
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-04-07 15:02:39 +0100

    Add wrapper class

--- sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_threecol_3_6_3_stacked/bootstrap-threecol-3-6-3-stacked.tpl.php
+++ sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_threecol_3_6_3_stacked/bootstrap-threecol-3-6-3-stacked.tpl.php
@@ -1,10 +1,10 @@
 <div class="panel-display bootstrap-threecol-3-6-3" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row-fluid">
+  <div class="row-fluid top-wrapper">
     <div class="panel-panel span12">
       <?php print $content['top']; ?>
     </div>
   </div>
-  <div class="row-fluid">
+  <div class="row-fluid center-wrapper">
     <div class="panel-panel span3">
       <?php print $content['left']; ?>
     </div>
@@ -15,7 +15,7 @@
       <?php print $content['right']; ?>
     </div>
   </div>
-  <div class="row-fluid">
+  <div class="row-fluid bottom-wrapper">
     <div class="panel-panel span12">
       <?php print $content['bottom']; ?>
     </div>
--- sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_6_6_stacked/bootstrap-twocol-6-6-stacked.tpl.php
+++ sites/all/modules/contrib/panels_bootstrap_layouts/plugins/layouts/bootstrap_twocol_6_6_stacked/bootstrap-twocol-6-6-stacked.tpl.php
@@ -1,10 +1,10 @@
 <div class="panel-display bootstrap-twocol-6-6-stacked" <?php if (!empty($css_id)) { print "id=\"$css_id\""; } ?>>
-  <div class="row-fluid">
+  <div class="row-fluid top-wrapper">
     <div class="panel-panel span12">
       <?php print $content['top']; ?>
     </div>
   </div>
-  <div class="row-fluid">
+  <div class="row-fluid center-wrapper">
     <div class="panel-panel span6">
       <?php print $content['left']; ?>
     </div>
@@ -12,7 +12,7 @@
       <?php print $content['right']; ?>
     </div>
   </div>
-  <div class="row-fluid">
+  <div class="row-fluid bottom-wrapper">
     <div class="panel-panel span12">
       <?php print $content['bottom']; ?>
     </div>
