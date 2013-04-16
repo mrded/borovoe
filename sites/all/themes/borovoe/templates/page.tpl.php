6ad3da570cb13479cbbf3a200acbd9f2fea33a8b
Dmitry Demenchuk <dmitry@demenchuk.me>
2013-04-16 20:52:37 +0100

    change something.

--- sites/all/themes/borovoe/templates/page.tpl.php
+++ sites/all/themes/borovoe/templates/page.tpl.php
@@ -3,6 +3,10 @@
   <div class="mountains right"></div>
   <div class="clouds left"></div>
   <div class="clouds right"></div>
+
+  <div class="page-header">
+    <h1>Боровое <small>Subtext for header</small></h1>
+  </div>
 </div>
 
 <div class="super-style"><div class="horizon"></div></div>
@@ -20,26 +24,27 @@
         </div>
         <div class="span8">
           <section>
-            <?php if (!empty($page['highlighted'])): ?>
-              <div class="highlighted hero-unit"><?php print render($page['highlighted']); ?></div>
-            <?php endif; ?>
-            <?php if (!empty($breadcrumb)): print $breadcrumb; endif;?>
             <a id="main-content"></a>
             <?php print render($title_prefix); ?>
             <?php if (!empty($title)): ?>
-              <h1 class="page-header"><?php print $title; ?></h1>
+              <h2 class="page-header"><?php print $title; ?></h2>
             <?php endif; ?>
             <?php print render($title_suffix); ?>
+
             <?php print $messages; ?>
+
             <?php if (!empty($tabs)): ?>
               <?php print render($tabs); ?>
             <?php endif; ?>
+
             <?php if (!empty($page['help'])): ?>
               <div class="well"><?php print render($page['help']); ?></div>
             <?php endif; ?>
+
             <?php if (!empty($action_links)): ?>
               <ul class="action-links"><?php print render($action_links); ?></ul>
             <?php endif; ?>
+
             <?php print render($page['content']); ?>
           </section>
           <!--Body content-->
