847a03ea4b3f62ae61fc24ee656087fa2b603e2b
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-10-09 23:54:40 +0100

    Clean up.

--- sites/all/modules/contrib/views_bootstrap/templates/thumbnail/views-bootstrap-thumbnail-plugin-style.tpl.php
+++
@@ -1,9 +0,0 @@
-<ul id="views-bootstrap-thumbnail-<?php print $id ?>" class="<?php print $classes ?>">
-  <?php foreach ($rows as $key => $row): ?>
-    <li class="<?php print $column_type ?>">
-      <div class="thumbnail">
-        <?php print $row ?>
-      </div>
-    </li>
-  <?php endforeach ?>
-</ul>
--- sites/all/modules/contrib/views_stapel/templates/views-stapel-plugin-rows.tpl.php
+++
@@ -1,8 +0,0 @@
-<li data-pile="<?php print $stapel_group ?>">
-  <a href="node/<?php print $row->nid ?>">
-    <span class="tp-info">
-      <span><?php print $stapel_title ?></span>
-    </span>
-    <?php print $stapel_image ?>
-  </a>
-</li>
