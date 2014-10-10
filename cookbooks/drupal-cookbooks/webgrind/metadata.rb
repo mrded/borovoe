13921ea6ccfd4b6b12cb57f03ffb75a6e1fccf6f
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-10-10 02:08:46 +0100

    Update drupal and delete vagrant

--- cookbooks/drupal-cookbooks/webgrind/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer        "Glenn Prtt"
-maintainer_email  "glenn@allplayers.com"
-license           "Apache 2.0"
-description       "Installs webgrind PHP profile visualization tool."
-version           "0.1"
-recipe            "webgrind::default", "Installs webgrind"
