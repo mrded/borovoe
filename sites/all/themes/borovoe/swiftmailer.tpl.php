d994c424195c1acef0de9a60cd852f2068e6b0c0
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-10-12 18:32:11 +0100

    Wrap content to table

--- sites/all/themes/borovoe/swiftmailer.tpl.php
+++ sites/all/themes/borovoe/swiftmailer.tpl.php
@@ -117,7 +117,37 @@
 
 
           <!-- MODULE CONTENT ROW // -->
-          <?php print $body; ?>
+          <tr>
+            <td align="center" valign="top">
+              <!-- CENTERING TABLE // -->
+              <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                <tr>
+                  <td align="center" valign="top">
+                    <!-- FLEXIBLE CONTAINER // -->
+                    <table border="0" cellpadding="0" cellspacing="0" width="600" class="flexibleContainer">
+                      <tr>
+                        <td align="center" valign="top" width="600" class="flexibleContainerCell">
+
+                          <!-- CONTENT TABLE // -->
+                          <table border="0" cellpadding="0" cellspacing="0" width="100%">
+                            <tr>
+                              <td valign="top" class="textContent">
+                                <?php print $body; ?>
+                              </td>
+                            </tr>
+                          </table>
+                          <!-- // CONTENT TABLE -->
+
+                        </td>
+                      </tr>
+                    </table>
+                    <!-- // FLEXIBLE CONTAINER -->
+                  </td>
+                </tr>
+              </table>
+              <!-- // CENTERING TABLE -->
+            </td>
+          </tr>
           <!-- // MODULE CONTENT ROW -->
 
           <!-- MODULE FOOTER ROW // -->
