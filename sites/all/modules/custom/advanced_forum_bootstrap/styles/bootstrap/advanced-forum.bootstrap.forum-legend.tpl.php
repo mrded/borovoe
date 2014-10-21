e9991ab640d359cf87f5c2210abf10d470aa154a
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-10-21 22:30:07 +0100

    Add forum-legend template

---
+++ sites/all/modules/custom/advanced_forum_bootstrap/styles/bootstrap/advanced-forum.bootstrap.forum-legend.tpl.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @file
+ * Theme implementation to show forum legend.
+ *
+ */
+?>
+
+<div class="well">
+  <ul class="list-unstyled">
+    <li>
+      <span class="glyphicon glyphicon-star"></span> <?php print t('New posts'); ?>
+    </li>
+    <li>
+      <span class="glyphicon glyphicon-star-empty"></span> <?php print t('No new posts'); ?>
+    </li>
+  </ul>
+</div>
