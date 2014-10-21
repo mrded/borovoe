c0e5ec9d04408e795f95fbd00ff89b7ac663f8e4
Dmitry Demenchuk <dmitry@demenchuk.me>
2014-10-21 22:51:26 +0100

    Update template

--- sites/all/modules/custom/advanced_forum_bootstrap/styles/bootstrap/advanced-forum.bootstrap.forum-legend.tpl.php
+++ sites/all/modules/custom/advanced_forum_bootstrap/styles/bootstrap/advanced-forum.bootstrap.forum-legend.tpl.php
@@ -7,13 +7,11 @@
  */
 ?>
 
-<div class="well">
-  <ul class="list-unstyled">
-    <li>
-      <span class="glyphicon glyphicon-star"></span> <?php print t('New posts'); ?>
-    </li>
-    <li>
-      <span class="glyphicon glyphicon-star-empty"></span> <?php print t('No new posts'); ?>
-    </li>
-  </ul>
-</div>
+<ul class="list-unstyled">
+  <li>
+    <span class="glyphicon glyphicon-star"></span> <?php print t('New posts'); ?>
+  </li>
+  <li>
+    <span class="glyphicon glyphicon-star-empty"></span> <?php print t('No new posts'); ?>
+  </li>
+</ul>
